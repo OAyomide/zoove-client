1d1b25bc3b064d2b16a0c79cbf947b12fb723602
Ayomide Onigbinde <onigbindeayomide@gmail.com>
2020-11-27 19:49:17 +0100

    fix

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -316,7 +316,7 @@ function Home() {
         return
       }
 
-      console.log('Outgoing socket message is: ', socketData)
+      // console.log('Outgoing socket message is: ', socketData)
       socket.send(JSON.stringify(socketData))
     } catch (error) {
       console.log(`Error fetching here`)
